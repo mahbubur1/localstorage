fd90158de8e13e719749c96ace50c4008400833f
mahbubur1 <mdmahbuborr462@gail.com>
2022-03-28 01:09:21 +0600

    get data frome local storage

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
-const familyMember = {};
+let familyMember = {};
 
 const addFamilyMember = member => {
+    const storeMember = localStorage.getItem('allMembers');
+    if(storeMember){
+        familyMember = JSON.parse(storeMember);
+    }
     if(member in familyMember){
         familyMember[member] = familyMember[member] + 1;
     }
     else{
         familyMember[member] = 1;
     }
-    console.log(familyMember);
+    localStorage.setItem('allMembers',JSON.stringify(familyMember));
 }
